fedf2e06786cb86a2a205537908efa9194b76052
Tori <Tori7809@gmail.com>
2019-08-28 13:45:57 -0600

    Tigger Events And Float Data
    Made Scripts. Placed a player and made a trigger event to add and subtract health

---
+++ 2019Scripting/Assets/TriggerEventBehavior.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventBehavior : MonoBehaviour
+{
+    public UnityEvent TriggerEnterEvent, TriggrtEnterStay;
+    private void OnTriggerEnter(Collider other)
+    {
+     TriggerEnterEvent.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        TriggerStayEvent.Invoke();
+    }
+
+    
+    }
+}
